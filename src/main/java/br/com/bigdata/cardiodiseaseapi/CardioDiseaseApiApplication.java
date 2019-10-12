cac373627aef11c8eeaf0c26bd292872b2537047
Yan Alexander Venera <yan.venera@reclameaqui.com.br>
2019-10-11 22:19:24 -0300

    first commit

---
+++ src/main/java/br/com/bigdata/cardiodiseaseapi/CardioDiseaseApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.bigdata.cardiodiseaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardioDiseaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardioDiseaseApiApplication.class, args);
+	}
+
+}
